9b9f201e9d5a2a5184d82147fd8456be56b3398a
zengchzh <825597202@qq.com>
2021-03-10 10:38:43 +0800

    init zrpc

---
+++ src/main/java/com/zchzh/zrpc/ZrpcApplication.java
@@ -0,0 +1,13 @@
+package com.zchzh.zrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZrpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zchzh/zrpc/ZrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zchzh.zrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
